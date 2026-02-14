9dd4006b2e58af83b685f264a70ad99c86e0ce0e
gokul77898 <gokulhacker669@gmail.com>
2026-02-14 18:38:32 +0530

    Replace nav menu preview images with SVG placeholders

--- src/components/header/config.ts
+++ src/components/header/config.ts
@@ -4,27 +4,27 @@ const links: Link[] = [
   {
     title: 'Home',
     href: '/',
-    thumbnail: '/assets/nav-link-previews/landing.png'
+    thumbnail: '/assets/nav-link-previews/landing.svg'
   },
   {
     title: 'About',
     href: '/#about',
-    thumbnail: '/assets/nav-link-previews/about.png'
+    thumbnail: '/assets/nav-link-previews/about.svg'
   },
   {
     title: 'Skills',
     href: '/#skills',
-    thumbnail: '/assets/nav-link-previews/skills.png'
+    thumbnail: '/assets/nav-link-previews/skills.svg'
   },
   {
     title: 'Projects',
     href: '/#projects',
-    thumbnail: '/assets/nav-link-previews/projects.png'
+    thumbnail: '/assets/nav-link-previews/projects.svg'
   },
   // {
   //   title: 'Skills',
   //   href: '/skills',
-  //   thumbnail: '/assets/nav-link-previews/skills.png'
+  //   thumbnail: '/assets/nav-link-previews/skills.svg'
   // },
   // {
   //   title: 'Testimonials',
@@ -34,12 +34,12 @@ const links: Link[] = [
   {
     title: 'Blogs',
     href: '/blogs',
-    thumbnail: '/assets/nav-link-previews/blog.png',
+    thumbnail: '/assets/nav-link-previews/blog.svg',
   },
   {
     title: 'Contact',
     href: '/#contact',
-    thumbnail: '/assets/nav-link-previews/contact.png'
+    thumbnail: '/assets/nav-link-previews/contact.svg'
   }
 ];
 
